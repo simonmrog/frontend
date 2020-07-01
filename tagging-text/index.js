5736f5e561a69107ab5d3cd81c683c6ae911f354
simonmrog <simonmrog@gmail.com>
2020-06-30 20:51:51 -0500

    tagging removal implemented

--- tagging-text/index.js
+++ tagging-text/index.js
@@ -2,20 +2,41 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const text = document.querySelector(".text");
   const colors = ["orange", "red", "green", "darkgreen", "cyan"];
+  let labelsNumber = 0;
+
+  function removeTag(e) {
+    const el = this;
+    const remove = confirm("Do you want to remove this tag?");
+    if (remove) {
+      const textContent = el.textContent.replace("TAG", "").trim();
+      text.innerHTML = text.innerHTML.replace(el.outerHTML, textContent);
+    }
+  }
+
+  function addTagEvents() {
+    const tags = document.querySelectorAll("mark.tag");
+    tags.forEach(function(tag) {
+      tag.addEventListener("click", removeTag);
+    });
+  }
 
   function tagSelection() {
     const selection = window.getSelection();
     let range = selection.getRangeAt(0);
     const text = range.toString();
     if (text[0] === " ") range.setStart(range.startContainer, range.startOffset + 1);
-    if (text[text.length - 1] === " ") if (text[0] === " ") range.setEnd(range.endContainer, range.endOffset - 1);
+    if (text[text.length - 1] === " ") range.setEnd(range.endContainer, range.endOffset - 1);
     if (text !== "") {
-      const span = document.createElement("strong")
-      span.innerHTML = `<span class="tag" style="background-color: ${colors[Math.floor(5 * Math.random())]}">${range}</span>`
+      const span = document.createElement("mark")
+      span.setAttribute("class", "tag");
+      span.setAttribute("id", labelsNumber++);
+      span.style.backgroundColor = colors[Math.floor(5 * Math.random())];
+      span.innerHTML = range + "<span> TAG</span>";
       range.deleteContents();
       range.insertNode(span)
     }
     selection.removeAllRanges();
+    addTagEvents();
   }
 
   text.addEventListener("mouseup", tagSelection);
